25639141e093cf86ea35cead04505e0abec2d62a
Amjad <amjadmaqsuma@gmail.com>
2022-04-06 19:31:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards( namesArray, action ) {
+    let thankYouCards = [];
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${action} gift!` )
+    }
+    return thankYouCards
+  }
+  
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
+  console.log(writeCards( ["Khalil","Amjad"], "Graduation" ));
+  countDown(10);
